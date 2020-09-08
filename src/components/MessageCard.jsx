3d5087c1e96ab0e3592f4c256be11476d65d187c
Rosalyn Land <roz.s.land@gmail.com>
2020-09-08 13:04:26 +0100

    create messagecard component

---
+++ src/components/MessageCard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const MessageCard = () => {
+  return (
+    <div>
+      <img src="" alt="userpic" />
+      <h3>Helen</h3>
+      <p>I am a message</p>
+    </div>
+  );
+};
+
+export default MessageCard;
