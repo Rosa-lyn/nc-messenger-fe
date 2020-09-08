b6a5610795f30365be91f56e9177e20510c094d5
Rosalyn Land <roz.s.land@gmail.com>
2020-09-08 13:03:03 +0100

    add components and router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from "react";
 import "./App.css";
+import { Router } from "@reach/router";
+import Header from "./components/Header";
+import MessageList from "./components/MessageList";
+import UserProfile from "./components/UserProfile";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <Router>
+        <MessageList path="/" />
+        <UserProfile path="/user" />
+      </Router>
+    </div>
+  );
 }
 
 export default App;
