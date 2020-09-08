fdd444f0bdf40ef0f37b7021c175f657db5f3743
Rosalyn Land <roz.s.land@gmail.com>
2020-09-08 13:03:30 +0100

    create header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>NC Messenger</h1>
+      <span role="img" aria-label="menu">
+        🍔
+      </span>
+    </header>
+  );
+};
+
+export default Header;
